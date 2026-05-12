a026d524233dbaec5920f7e7c53a1fa5084686ee
Adam-Dulc <Adam-Dulc@hotmail.com>
2026-05-12 10:07:45 +0100

    Seperating HTML, CSS, and JavaScript
    Seperating files

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,26 @@
 let js = "amazing";
-if (js === "amazing") alert("hello world");
+// if (js === "amazing") alert("hello world");
 console.log(40 + 8 + 23 - 10);
+
+let firstName = "Adam";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+const PI = 3.1415;
+
+let answer = PI * 5;
+console.log(answer);
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Junior Web Dev";
+
+console.log(`I used to be a ${myFirstJob}, but now I'm a ${myCurrentJob}`);
+
+let isIsland = true;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
