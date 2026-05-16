1142707207520e6670e5be80f530ef2338fa0321
Adam-Dulc <Adam-Dulc@hotmail.com>
2026-05-16 18:06:25 +0100

    Function Types
    Playing with different function types

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,17 +1,39 @@
-const age = 19;
+/*========================*/
+/*  Function Declaration  */
+/*========================*/
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
+}
+const age1 = calcAge1(1991);
 
-age >= 18
-  ? console.log(`I like to drink wine 🍷`)
-  : console.log(`I like to drink water 💧`);
+/*=======================*/
+/*  Function Expression  */
+/*=======================*/
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+const age2 = calcAge2(1991);
 
+console.log(age1, age2);
 
+/*=======================*/
+/*     Arrow Function    */
+/*=======================*/
 
-let population = 1600
+const calcAge3 = (birthYear) => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
 
-population < 1800 ? console.log("It's below 1800") :
-population === 1800 ? console.log("It's equal to 1800") :
-console.log("It's above 1800");
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  // return retirement
+  return `${firstName} retires in ${retirement} years`;
+};
 
+console.log(yearsUntilRetirement(1991, 'Adam'));
+console.log(yearsUntilRetirement(1980, 'Bob'));
 
-
-
+/*====================================*/
+/*     Functions Calling Functions    */
+/*====================================*/
