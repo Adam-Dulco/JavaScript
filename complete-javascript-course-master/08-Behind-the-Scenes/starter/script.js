b0ce919adcc1a677d97b1b90b0a1bf9ac9cd4931
Adam-Dulc <Adam-Dulc@hotmail.com>
2026-07-13 14:47:28 +0100

    Hoisting and TDZ (Temporal Dead Zone) practice
    logging variables to the console to show hoisting and Temporal Dead Zone activity

--- complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -1,38 +1,84 @@
 'use strict';
 
-function calcAge(birthYear) {
-  const age = 2037 - birthYear;
+// function calcAge(birthYear) {
+//   const age = 2037 - birthYear;
 
-  function printAge() {
-    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
-    console.log(output);
+//   function printAge() {
+//     let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+//     console.log(output);
 
-    if (birthYear >= 1981 && birthYear <= 1996) {
-      var millenial = true;
-      // Creating NEW variable with same name as outer scope's variable
-      const firstName = 'Steven';
+//     if (birthYear >= 1981 && birthYear <= 1996) {
+//       var millenial = true;
+//       // Creating NEW variable with same name as outer scope's variable
+//       const firstName = 'Steven';
 
-      // Reassigning outer scope's variable.
-      output = 'NEW OUTPUT!';
+//       // Reassigning outer scope's variable.
+//       output = 'NEW OUTPUT!';
 
-      const str = `Oh, and you are a millenial ${firstName}`;
-      console.log(str);
+//       const str = `Oh, and you are a millenial ${firstName}`;
+//       console.log(str);
 
-      function add(a, b) {
-        return a + b;
-      }
-    }
-    // console.log(str);
-    console.log(millenial);
-    // console.log(add(2, 3));
-    console.log(output);
-  }
-  printAge();
-  return age;
-}
+//       function add(a, b) {
+//         return a + b;
+//       }
+//     }
+//     // console.log(str);
+//     console.log(millenial);
+//     // console.log(add(2, 3));
+//     console.log(output);
+//   }
+//   printAge();
+//   return age;
+// }
 
-const firstName = 'Adam';
-calcAge(1991);
+// const firstName = 'Adam';
+// calcAge(1991);
 
-// console.log(age)
-// printAge()
+// // console.log(age)
+// // printAge()
+
+// HOISTING
+
+// Variables
+// console.log(me);
+// console.log(job);
+// console.log(year);
+
+// var me = 'Adam';
+// let job = 'teacher';
+// const year = 1991;
+
+// Functions
+// console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+// console.log(addDecl(2, 3));
+
+// function addDecl(a, b) {
+//   return a + b;
+// }
+
+// const addExpr = function (a, b) {
+//   return a + b;
+// };
+
+// var addArrow = (a, b) => a + b;
+
+// Example
+
+// console.log(!numProducts);
+
+// if (!numProducts) deleteShoppingCart();
+
+// var numProducts = 10;
+
+// function deleteShoppingCart() {
+//   console.log('All products deleted!');
+// }
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log( x === window.x)
+console.log( x === window.y)
+console.log( x === window.x)
