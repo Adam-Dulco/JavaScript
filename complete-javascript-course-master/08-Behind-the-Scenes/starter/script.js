587dd54a0c3d811b2dde36b5c4f455936c2f7376
Adam-Dulc <Adam-Dulc@hotmail.com>
2026-07-15 12:08:47 +0100

    Regular Functions vs Arrow Functions
    Replace 'this' binding examples with new section demonstrating the 'arguments' keyword. Shows how regular function expressions have access to the arguments object, contrasting with arrow functions which do not. Gained an understanding of the 'this' keyword being used un objects, standard functions and arrow functions.

--- complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -105,25 +105,89 @@
 
 // calcAgeArrow(1980);
 
-const jonas = {
-  name: 'Jonas',
-  year: 1991,
-  calcAge: function () {
-    console.log(this);
-    console.log(2037 - this.year);
-    console.log(this.name);
-  },
+// const jonas = {
+//   name: 'Jonas',
+//   year: 1991,
+//   calcAge: function () {
+//     console.log(this);
+//     console.log(2037 - this.year);
+//     console.log(this.name);
+//   },
+// };
+
+// jonas.calcAge();
+
+// const matilda = {
+//   name: 'Matilda',
+//   year: 2017,
+// };
+
+// matilda.calcAge = jonas.calcAge;
+// matilda.calcAge();
+
+// const f = jonas.calcAge;
+// f();
+
+// var firstName = 'Geoff';
+
+// const jonas = {
+//   firstName: 'Jonas',
+//   year: 1991,
+//   calcAge: function () {
+//     // console.log(this);
+//     console.log(2037 - this.year);
+
+//     // SOLUTION 1
+//     // const self = this; // self or that
+//     // const isMillenial = function () {
+//     //   console.log(self);
+//     //   console.log(self.year >= 1981 && self.year <= 1996);
+//     //   // console.log(this.year >= 1981 && this.year <= 1996);
+//     // };
+
+//     // SOLUTION 2
+//     const isMillenial = () => {
+//       console.log(this);
+//       console.log(this.year >= 1981 && this.year <= 1996);
+//       // console.log(this.year >= 1981 && this.year <= 1996);
+//     };
+
+//     isMillenial();
+//   },
+
+//   // Arrow function USED as a method does NOT get 'this' from the jonas object.
+//   // It uses 'this' from the surrounding scope (often window in a browser script).
+//   arrowGreet: () => {
+//     console.log(`Hey ${this.firstName}`);
+//     console.log(this);
+//   },
+
+//   // Regular function used as a method gets 'this' from the object calling it.
+//   // Here, jonas.greet() makes 'this' equal to jonas.
+//   greet: function () {
+//     console.log(`Hey ${this.firstName}`);
+//     console.log(this);
+//   },
+// };
+
+// jonas.arrowGreet();
+// jonas.greet();
+// jonas.calcAge();
+
+// ARGUMENTS KEYWORD
+
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
 };
 
-jonas.calcAge();
+addExpr(2, 5);
+addExpr(2, 5, 8, 12);
 
-const matilda = {
-  name: 'Matilda',
-  year: 2017,
+var addArrow = (a, b) => {
+  a + b;
+  console.log(addArrow);
+  return a + b;
 };
 
-matilda.calcAge = jonas.calcAge;
-matilda.calcAge();
-
-const f = jonas.calcAge;
-f();
+addArrow(2, 5, 8);
