ad9ef8f3f87af2f23c671f574b160e6f13defd40
Adam D <Adam-Dulc@hotmail.com>
2026-07-28 15:29:54 +0100

    Refactor Map examples; add Coding Challenge #3
    Reorganized and commented Map examples (iteration and fundamentals), consolidated quiz/demo snippets, and cleaned up stray console logs. Implemented Coding Challenge #3: added a gameEvents Map, created deduplicated events array, removed the unfair 64th-minute event, logged average event frequency, and printed each event labeled as FIRST or SECOND HALF.

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -83,40 +83,90 @@ const restaurant = {
 };
 
 ///////////////////////////////////////
-// MAPS
+// MAPS: Iteration
 
-const rest = new Map();
-rest.set('name', 'Classico Italiano');
-rest.set(1, 'Firenze, Italy');
+// const question = new Map([
+//   ['question', 'What is the best programming language in the world?'],
+//   [1, 'C'],
+//   [2, 'Java'],
+//   [3, 'JavaScript'],
+//   ['correct', 3],
+//   [true, 'Correct 🎉'],
+//   [false, 'Try again!'],
+// ]);
 
-rest
-  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
-  .set('open', 11)
-  .set('close', 23)
-  .set(true, 'We are open :D')
-  .set(false, 'We are closed :(');
+// console.log(question);
 
-// rest.clear()
+// // Convert object to map
+// console.log(Object.entries(openingHours));
+
+// const hoursMap = new Map(Object.entries(openingHours));
+// console.log(hoursMap);
+
+// Quiz app
+// console.log(question.get('question'));
+// for (const [key, value] of question) {
+//   if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+// }
+
+// const answer = Number(prompt('Your answer'));
+// if (answer === 1 || answer === 2) {
+//   console.log(question.get(false));
+// } else if (answer === 3) {
+//   console.log(question.get(true));
+// } else {
+//   console.log(
+//     'This is an insufficient answer, please choose one of the 3 options',
+//   );
+// }
+
+// console.log(question.get(question.get('correct') === answer));
+
+// Convert map to array
+// console.log([...question]);
+// console.log([...question.entries()]);
+// console.log([...question.keys()]);
+// console.log([...question.values()]);
+
+// for (const [key, value] of question) {
+//   console.log(value);
+// }
+
+///////////////////////////////////////
+// MAPS: Fundamentals
+
+// const rest = new Map();
+// rest.set('name', 'Classico Italiano');
+// rest.set(1, 'Firenze, Italy');
+
+// rest
+//   .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+//   .set('open', 11)
+//   .set('close', 23)
+//   .set(true, 'We are open :D')
+//   .set(false, 'We are closed :(');
+
+// // rest.clear()
+// // console.log(rest.size);
+
+// const arr = [1, 2];
+// rest.set(arr, 'Test');
+// console.log(rest.get(arr));
+
+// rest.set(document.querySelector('h1'), 'Heading');
+// console.log(rest);
+
+// console.log(rest.get('name'));
+// console.log(rest.get(1));
+// console.log(rest.get(true));
+
+// const time = 8;
+// console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+// console.log(rest.has('categories'));
+// rest.delete('name');
+// console.log(rest);
 // console.log(rest.size);
-
-const arr = [1, 2];
-rest.set(arr, 'Test');
-console.log(rest.get(arr));
-
-rest.set(document.querySelector('h1'), 'Heading');
-console.log(rest);
-
-console.log(rest.get('name'));
-console.log(rest.get(1));
-console.log(rest.get(true));
-
-const time = 8;
-console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
-
-console.log(rest.has('categories'));
-rest.delete('name');
-console.log(rest);
-console.log(rest.size);
 
 ///////////////////////////////////////
 // New Operations to make sets useful
@@ -717,3 +767,52 @@ BONUS: Create an object called 'scorers' which contains the names of the players
 
 GOOD LUCK 😀
 */
+
+///////////////////////////////////////
+// Coding Challenge #3
+
+/* 
+Let's continue with our football betting app! This time, we have a map with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
+
+1. Create an array 'events' of the different game events that happened (no duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+      [FIRST HALF] 17: ⚽️ GOAL
+
+GOOD LUCK 😀
+*/
+
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+// 1.
+let events = [...new Set(gameEvents.values())];
+console.log(events);
+
+// 2.
+gameEvents.delete(64);
+console.log(gameEvents);
+
+// 3.
+console.log('An event happened, on average, every 9 minutes');
+
+// 4.
+for (const [key, value] of gameEvents) {
+  if (key <= 45) {
+    console.log(`[FIRST HALF], in minute ${key}: ${value}`);
+  } else if (key > 45) {
+    console.log(`[SECOND HALF], in minute ${key}: ${value}`);
+  }
+}
